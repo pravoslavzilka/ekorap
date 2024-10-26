8d6176872c4ae358a594e750f8235f9d989e9867
pravoslavzilka <pravoslav.zilka@gmail.com>
2024-10-27 01:29:34 +0200

    Recieving, confirming orders

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from openai import OpenAI
 from threading import Thread
 from itsdangerous import URLSafeTimedSerializer
 from flask_mail import Mail, Message
+
+from database import db_session
 from models import *
 
 
@@ -17,7 +19,6 @@ app.config["MAIL_USERNAME"] = 'ekolo.bratislava@gmail.com'
 app.config["MAIL_PASSWORD"] = 'yqnp fitd demx ymzx '
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
-#app.config['MAIL_DEFAULT_SENDER '] = "no-reply@gma.sk"
 
 
 mail = Mail(app)
@@ -40,15 +41,102 @@ def matching_algo(input_data):
         for item in items:
             print(item.name)
 
-    return "0"
+    return 0
 
 
+@app.route("/receive")
+def receive_order():
+    data = {
+      "organization_email": "pravoslav.zilka@gmail.com",
+      "products": [
+        {
+          "name": "lamp",
+          "reserved_amount": 50
+        },
+        {
+          "name": "beds",
+          "reserved_amount": 50
+        }
+      ]
+    }
+
+    email = data["organization_email"]
+    products_dict = {product["name"]: product["reserved_amount"] for product in data["products"]}
+
+    list_of_items = ""
+
+    u = User.query.filter(User.email == email).first()
+    order = Order()
+    order.user = u
+    for product, value in products_dict.items():
+        order_item = OrderItem(product, value)
+        order_item.order = order
+        db_session.add(order_item)
+        list_of_items += f"\n{product} of value {value} "
+
+    db_session.add(order)
+    db_session.commit()
+
+    #send email
+
+    subject = f"The {u.name} organization have sent material request"
+    body = "Organisation wants to have these items: " + list_of_items + "\n Order Id: " + order.id
+    recipient = u.email
+
+    email_send(subject, body, recipient)
+
+
+    return "success"
+
+
+@app.route("/confirm-order")
+def confirm_order():
+    data = {
+        "order_id": "12",
+        "organization_email": "pravoslav.zilka@gmail.com",
+        "products": [
+            {
+                "name": "lamp",
+                "reserved_amount": 50
+            },
+            {
+                "name": "beds",
+                "reserved_amount": 50
+            }
+        ]
+    }
+
+    order_id = data["order_id"]
+    products_dict = {product["name"]: product["reserved_amount"] for product in data["products"]}
+
+    order = Order.query.filter(Order.id == order_id).first()
+    for item in order.items():
+        for product, value in products_dict.values():
+            if product == item.name:
+                item.count = value
+
+    db_session.commit()
+
+    subject = "Your material request have been confirmed"
+    body = "Please pick it up on 11 of November 2024 "
+    recipient = order.user.email
+
+    email_send(subject, body, recipient)
 
 
 
 def send_email(app, msg):
     with app.app_context():
         mail.send(msg)
+
+
+
+def email_send(subject, body, recipient):
+
+    msg = Message(subject=subject, sender="ekolo.bratislava@gmail.com", recipients=[recipient])
+    msg.body = body
+    mail.send(msg)
+
 
 
 @app.route("/email/")
